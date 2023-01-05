2b9e94cf8df07cc9249a309962c643ecc065e010
BALA MURALI KRISHNA BIYYAPU <bbmkr@KRISHNA>
2023-01-05 21:30:26 +0530

    authentication implementation using  jwt

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require("dotenv").config({ path: ".env" });
+const bcrypt = require("bcrypt");
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
 const Restaurant = require("./Models/restaurant.js");
@@ -6,9 +9,75 @@ const Dish = require("./Models/dish.js");
 const User = require("./Models/user.js");
 const Order = require("./Models/order.js");
 const Orderitems = require("./Models/orderitems.js");
-const md5 = require("md5");
+const { Op } = require("sequelize");
 var cors = require("cors");
 app.use(cors());
+
+app.post("/signup", async function (req, res) {
+  try {
+    await User.findOne({
+      where: {
+        [Op.or]: [{ name: req.body.name }, { phone: req.body.phone }],
+      },
+    }).then(async (data) => {
+      if (data === null) {
+        const name = req.body.name;
+        const phone = req.body.phone;
+        const password = await bcrypt.hash(req.body.password, 10);
+        const user = { name: name, phone: phone, password: password };
+        User.create({ name: name, phone: phone, password: password })
+          .then((data) => res.json(data))
+          .catch((err) => res.status(500).send(err));
+      } else {
+        res.status(500).json({
+          msg: "User Already Exist, try to login if you are a Old user",
+        });
+      }
+    });
+  } catch {
+    res.status(500).json({ msg: "something went wrong!" });
+  }
+});
+
+app.post("/login", async function (req, res) {
+  await User.findOne({ where: { phone: req.body.phone } }).then(
+    async (data) => {
+      if (data === null) {
+        res
+          .status(500)
+          .send({ msg: "user doesnot exists, create a new account" });
+      } else {
+        try {
+          const { password } = req.body;
+          if (await bcrypt.compare(password, data.password)) {
+            const user = { id: data.id };
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+            res.json({ accessToken: accessToken });
+          } else {
+            res.json({ msg: "Auth Failed" });
+          }
+        } catch {
+          res.status(500).json({ msg: "something went wrong" });
+        }
+      }
+    }
+  );
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+app.use(authenticateToken);
 // restaurant routes
 app.post("/restaurants", function (req, res) {
   Restaurant.create(req.body)
@@ -71,32 +140,15 @@ app.delete("/dishes", function (req, res) {
 });
 
 app.get("/users", function (req, res) {
-  User.findAll().then((data) => res.json(data));
+  User.findAll({ where: { id: req.user.id } }).then((data) => {
+    res.json(data);
+  });
 });
 
-app.post("/signup", function (req, res) {
-  const { name, phone, password } = req.body;
-  User.create({ name: name, phone: phone, password: md5(password) })
-    .then((data) => res.json(data))
-    .catch((err) => res.json(err));
-});
-
-app.post("/login", function (req, res) {
-  const { phone, password } = req.body;
-  User.findOne({ where: { phone: phone } })
-    .then((data) => {
-      if (data.password === md5(password)) {
-        res.json(data);
-      } else {
-        res.status(401).json({ msg: "Auth Failed" });
-      }
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
 app.get("/orders", function (req, res) {
-  Order.findAll().then((data) => res.json(data));
+  Order.findAll({ where: { userId: req.user.id } }).then((data) =>
+    res.json(data)
+  );
 });
 
 app.get("/orderitems", function (req, res) {
@@ -111,8 +163,7 @@ app.get("/orders/:orderId/orderitems", function (req, res) {
 });
 
 app.post("/orders", function (req, res) {
-  const userId = { userId: 1 };
-  Order.create(userId)
+  Order.create({ userId: req.user.id })
     .then(async (data) => {
       const orderItems = req.body.orderItems;
       for (const orderItem of orderItems) {
